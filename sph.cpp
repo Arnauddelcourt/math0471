e09649f575162235fc873c185711f6eec3073382
Romain Boman <romain.boman@gmail.com>
2017-02-09 15:00:55 +0100

    version -1

---
+++ sph.cpp
@@ -0,0 +1,14 @@
+#include "sph.h"
+
+int main(int argc, char *argv[])
+{
+    std::vector<double[3]> pos;
+    
+    
+
+
+
+
+
+    return 0;
+}
---
+++ sph.h
@@ -0,0 +1,12 @@
+#ifndef SPH_H
+#define SPH_H
+
+void toparaview(std::string const &fname, 
+                std::vector<double> const &x,
+                std::vector<double> const &y);
+                
+
+
+
+#endif
+
