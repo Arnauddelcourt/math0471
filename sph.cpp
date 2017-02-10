d5ceaaec781fb880e415e9bc3de04bffacbe13ce
Romain Boman <romain.boman@gmail.com>
2017-02-10 15:35:07 +0100

    fake time-step loop

--- sph.cpp
+++ sph.cpp
@@ -11,27 +11,10 @@ int main(int argc, char *argv[])
     meshcube(o, L, s, pos);
 
     // creation of dummy pressure/density/velocity fields &
-    std::cout << "creation of dummy pressure/velocity fields\n";
     int nbp = pos.size()/3;
     std::vector<double> pressure(nbp);
     std::vector<double> density(nbp);
     std::vector<double> velocity(nbp*3);
-
-    for(int i=0; i<nbp; ++i)
-    {
-        double x = pos[3*i+0];
-        double y = pos[3*i+1];
-        double z = pos[3*i+2];
-
-        pressure[i] = z-10;
-        density[i] = x-10;
-        velocity[3*i+0] = x-(o[0]+L[0]/2);
-        velocity[3*i+1] = y-(o[1]+L[1]/2);
-        velocity[3*i+2] = z-(o[2]+L[2]/2);
-    }
-
-    // saving to paraview
-    std::cout << "saving data to paraview\n";
 
     std::map<std::string, std::vector<double> *> scalars;
     std::map<std::string, std::vector<double> *> vectors;
@@ -39,7 +22,30 @@ int main(int argc, char *argv[])
     scalars["density"]  = &density;
     vectors["velocity"] = &velocity;
 
-    paraview("results", pos, scalars, vectors);
+    // time step loop
+    for(int nstep=0; nstep<20; ++nstep)
+    {
+        double a = nstep/20.0*8*atan(1.0);
+
+        // generate dummy results
+        for(int i=0; i<nbp; ++i)
+        {
+            pos[3*i+2] -= 0.5/20;
+
+            double x = pos[3*i+0];
+            double y = pos[3*i+1];
+            double z = pos[3*i+2];
+
+            pressure[i] = z/2+sin(a);
+            density[i] = y+cos(a)+2;
+            velocity[3*i+0] = x-(o[0]+L[0]/2+L[0]*sin(a));
+            velocity[3*i+1] = y-(o[1]+L[1]/2+L[1]*cos(a));
+            velocity[3*i+2] = z-(o[2]+L[2]/2);
+        }
+
+        // save results to disk
+        paraview("results", nstep, pos, scalars, vectors);
+    }
 
     return 0;
 }
--- sph.h
+++ sph.h
@@ -5,13 +5,15 @@
 #include <vector>
 #include <iostream>
 #include <map>
-#include <math.h>
+#include <cmath>
+#include <cassert>
 
 void meshcube(double o[3], double L[3], double s, std::vector<double> &pos);
-void paraview(std::string const &filename,
+void paraview(std::string const &filename, 
+              int step,
               std::vector<double> const &pos,
               std::map<std::string, std::vector<double> *> const &scalars,
-              std::map<std::string, std::vector<double> *> const &vectors);         
+              std::map<std::string, std::vector<double> *> const &vectors);       
 
 
 
