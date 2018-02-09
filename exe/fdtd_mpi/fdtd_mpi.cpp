9f92372dbcd9cb4017c502727508ff0822376e40
Romain Boman <romain.boman@gmail.com>
2018-02-09 17:17:25 +0100

    uniform grid definition (to be continued)

--- exe/fdtd_mpi/fdtd_mpi.cpp
+++ exe/fdtd_mpi/fdtd_mpi.cpp
@@ -19,25 +19,31 @@
 
 using namespace vtl;
 
-void splitgrid(int np[3], int numprocs, int myid, int myn1[3], int myn2[3])
+void splitgrid(int np1[3], int np2[3], int numprocs, int myid, int myn1[3], int myn2[3])
 {
     int sdir = 0; // split direction
 
-    // calculate nb of points in each subdomain => mynp[]
-    int mynp;
+
+    // total nb of points in global grid
+    int np[3];
+    for (int i = 0; i < 3; ++i)
+        np[i] = np2[i] - np1[i] + 1;
+
+    // split grid along 'sdir'
     int nfloor = np[sdir] / numprocs;
     int rem = np[sdir] % numprocs;
     assert(np[sdir] == nfloor * numprocs + rem);
 
-    // build the point idx separating each subdomain
+    // build the list if idx separating each subdomain
     std::vector<int> nps(numprocs + 1);
-    nps[0] = 0;
+    nps[0] = np1[sdir];
     for (int i = 0; i < numprocs + 1; ++i)
     {
         nps[i + 1] = nps[i] + nfloor;
         if (i < rem)
             nps[i + 1]++;
     }
+
     /*
     if(myid==0) // DEBUG
     {
@@ -47,10 +53,11 @@ void splitgrid(int np[3], int numprocs, int myid, int myn1[3], int myn2[3])
         std::cout << "\n";
     }
     */
+
     for (int i = 0; i < 3; ++i)
     {
-        myn1[i] = 0;
-        myn2[i] = np[i] - 1;
+        myn1[i] = np1[i];
+        myn2[i] = np2[i];
     }
     myn1[sdir] = nps[myid];
     myn2[sdir] = nps[myid + 1] - 1;
@@ -72,7 +79,6 @@ int main(int argc, char *argv[])
 
     double o[3] = {10.0, 10.0, 10.0};
     double L[3] = {50.0, 60.0, 80.0};
-    //int np[3] = {51, 61, 81}; // nb of points in each direction - idx starting from 0 to np[i]-1
 
     int np1[3] = {0, 0, 0};    // first index
     int np2[3] = {50, 60, 80}; // last index
@@ -113,7 +119,7 @@ int main(int argc, char *argv[])
 
     // get my grid indices
     int myn1[3], myn2[3];
-    splitgrid(np, numprocs, myid, myn1, myn2);
+    splitgrid(np1, np2, numprocs, myid, myn1, myn2);
     std::cout << myid << ": ["
               << myn1[0] << '-' << myn2[0] << ", "
               << myn1[1] << '-' << myn2[1] << ", "
@@ -131,7 +137,7 @@ int main(int argc, char *argv[])
         for (int i = 0; i < numprocs; ++i)
         {
             int myn1[3], myn2[3];
-            splitgrid(np, numprocs, i, myn1, myn2);
+            splitgrid(np1, np2, numprocs, i, myn1, myn2);
             extents[i].resize(6);
             extents[i][0] = myn1[0];
             extents[i][1] = myn2[0];
@@ -146,15 +152,17 @@ int main(int argc, char *argv[])
     int mynbp = mynp[0] * mynp[1] * mynp[2];
 
     std::cout << myid << ": " << mynbp << " points created\n";
-    std::vector<double> Ez(mynbp);
-    std::vector<double> Hy(mynbp);
-    std::vector<double> poynting(mynbp * 3); // vector field
+    std::vector<double> scalarX(mynbp);
+    std::vector<double> scalarY(mynbp);
+    std::vector<double> scalarZ(mynbp);
+    std::vector<double> vectorSIN(mynbp * 3); // vector field
 
     std::map<std::string, std::vector<double> *> scalars;
     std::map<std::string, std::vector<double> *> vectors;
-    scalars["Ez"] = &Ez;
-    scalars["Hy"] = &Hy;
-    vectors["poynting"] = &poynting;
+    scalars["scalar X"] = &scalarX;
+    scalars["scalar Y"] = &scalarY;
+    scalars["scalar Z"] = &scalarZ;
+    vectors["vector SIN"] = &vectorSIN;
 
     // NOTE: the origin is the same for all subdomains
 
@@ -178,11 +186,12 @@ int main(int argc, char *argv[])
                     int idx = k * (mynp[1] * mynp[0]) + (j * mynp[0]) + i;
                     //idx++;
 
-                    Ez[idx] = x;
-                    Hy[idx] = y;
-                    poynting[idx * 3 + 0] = sin(2 * M_PI * (((x - (o[0] + L[0] / 2.)) / L[0]) + time));
-                    poynting[idx * 3 + 1] = cos(2 * M_PI * (((y - (o[1] + L[1] / 2.)) / L[1]) + time));
-                    poynting[idx * 3 + 2] = sin(2 * M_PI * (((z - (o[2] + L[2] / 2.)) / L[2]) + time));
+                    scalarX[idx] = x;
+                    scalarY[idx] = y;
+                    scalarZ[idx] = z;
+                    vectorSIN[idx * 3 + 0] = sin(2 * M_PI * (((x - (o[0] + L[0] / 2.)) / L[0]) + time));
+                    vectorSIN[idx * 3 + 1] = cos(2 * M_PI * (((y - (o[1] + L[1] / 2.)) / L[1]) + time));
+                    vectorSIN[idx * 3 + 2] = sin(2 * M_PI * (((z - (o[2] + L[2] / 2.)) / L[2]) + time));
                 }
             }
         }
