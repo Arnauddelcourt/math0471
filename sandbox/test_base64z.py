9edf0f1f67b4a1abf93402f8b9fe324074d711c6
Romain Boman <romain.boman@gmail.com>
2018-02-23 09:26:18 +0100

    prepare for testing

--- sandbox/test_base64z.py
+++ sandbox/test_base64z.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: latin-1; -*-
+# -*- coding: utf-8 -*-
 
 import base64
 z = base64.b64decode(r'AQAAAACAAACAEAAAYAAAAA==eJzt0rEJgDAQhtFlkkHsUwiC5MBCEATBTZKZcjNdJnCIvxK+4m3wPMrigplWSa6bxNou6cMkHodkplOS6yWxdkv6eCQer4i//OUvf/nLX/7yl7/85S9/+fuHvx9tUUBK')
--- sandbox/writevtk.py
+++ sandbox/writevtk.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: latin-1; -*-
+# -*- coding: utf-8 -*-
 # Write a (legacy) VTK file line by line from a set of points+values.
 # This program was used to build a paraview export routine in C++
 
